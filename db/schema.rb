0089c2f48ee7e7eb85b506810735cc2c873dd509
Raphaël Fischer <Raph@Raphaels-MacBook-Pro.local>
2014-03-20 23:41:13 -0700

    commit

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140319054616) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "place_types", force: true do |t|
     t.string   "name"
     t.string   "icon"
